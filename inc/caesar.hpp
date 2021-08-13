3f7e4d53cbbfff6184605df5a53985f43d29ca37
Jeremy Tieman <jtieman@brave.com>
2021-08-12 18:11:36 -0700

    Added Caesar Shift

---
+++ inc/caesar.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <sstream>
+
+namespace waves
+{
+  std::string caesar_shift(const std::string& s, const char shift)
+  {
+    std::stringstream out;
+
+    for(const auto c : s)
+    {
+      if(('A' <= c) && (c <= 'Z')) out << static_cast<char>((((c - 'A') + shift) % 26) + 'A');
+      else if(('a' <= c) && (c <= 'z')) out << static_cast<char>((((c - 'a') + shift) % 26) + 'a');
+    }
+
+    return out.str();
+  }
+}
---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include <caesar.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+
+namespace cryptoTest
+{
+  TEST_CASE("Verify Caesar Shift encrypts and decrypts correctly", "[caesar shift]")
+  {
+    REQUIRE(waves::caesar_shift("CAESAR", 3) == std::string("FDHVDU"));
+    REQUIRE(waves::caesar_shift("FDHVDU", -3) == std::string("CAESAR"));
+    REQUIRE(waves::caesar_shift("caesar", 3) == std::string("fdhvdu"));
+    REQUIRE(waves::caesar_shift("fdhvdu", -3) == std::string("caesar"));
+    REQUIRE(waves::caesar_shift("CAEsar", 3) == std::string("FDHvdu"));
+  }
+}
